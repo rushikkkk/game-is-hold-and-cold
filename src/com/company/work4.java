71e2178061b7e01536135730b1b4107c5ceb6bd6
rushikkkk <psgooooops@gmail.com>
2020-10-17 20:14:52 +0300

    game 1

---
+++ src/com/company/work4.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class work4 {
+    public static void main(String[] args) {
+        boolean op1 = false;
+        boolean op2 = true;
+        if(op1 == op2) {
+            System.out.println("true");
+        } else {
+            System.out.println("not equals ");
+        }
+    }
+}
